7abfdd5cf49199c666c4d22709f55accd6987033
Drew Hiller <ahiller0923@github.com>
2020-10-09 14:38:32 -0500

    Created a homepage for the application

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_home():
+    return render_template('Home.html')
+
+if __name__ == '__main__':
+    app.run()

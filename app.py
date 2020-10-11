b52077a81372ad66bf835aab46736a13870a8cf2
Andrew Hiller <ahiller0923@github.com>
2020-10-11 11:45:45 -0500

    Integrated some of the other updated code into this branch

--- app.py
+++ app.py
@@ -1,10 +1,60 @@
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+import os
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, g, redirect, url_for, render_template, flash
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def render_home():
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'account.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+))
+app.config.from_envvar('ACCOUNT_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('init_db')
+def init_db_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route('/home')
+def homepage():
     return render_template('Home.html')
+
 
 if __name__ == '__main__':
     app.run()
